b88a52e98dc827cf9239e09dc87ba8f727ce8894
Andrej Podhradsky <apodhrad@redhat.com>
2014-04-07 10:55:29 +0200

    Capture a screenshot when some shell remained open

--- plugins/org.jboss.reddeer.junit.extension/src/org/jboss/reddeer/junit/extension/after/test/impl/CloseAllShellsExt.java
+++ plugins/org.jboss.reddeer.junit.extension/src/org/jboss/reddeer/junit/extension/after/test/impl/CloseAllShellsExt.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import org.eclipse.swt.widgets.Shell;
 import org.jboss.reddeer.junit.extensionpoint.IAfterTest;
+import org.jboss.reddeer.junit.internal.screenshot.CaptureScreenshot;
+import org.jboss.reddeer.junit.internal.screenshot.CaptureScreenshotException;
 import org.jboss.reddeer.junit.logging.Logger;
 import org.jboss.reddeer.swt.handler.WidgetHandler;
 import org.jboss.reddeer.swt.impl.shell.WorkbenchShell;
@@ -65,7 +67,13 @@ public class CloseAllShellsExt implements IAfterTest {
 		@Override
 		protected void beforeShellIsClosed(Shell shell) {
 			super.beforeShellIsClosed(shell);
-			shellTitles.add(WidgetHandler.getInstance().getText(shell));
+			String shellTitle = WidgetHandler.getInstance().getText(shell); 
+			shellTitles.add(shellTitle);
+			try {
+				new CaptureScreenshot().captureScreenshot("openShell-" + shellTitle);
+			} catch (CaptureScreenshotException e) {
+				e.printStackTrace();
+			}
 		}
 
 	}
